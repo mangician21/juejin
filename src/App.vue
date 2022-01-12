0dda0861d4b4bdd065c7606d62d10fb495ebac71
magician21 <2235662800@qq.com>
2022-01-12 15:40:56 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,70 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script setup lang="ts">
+import ColumnList from './components/ColumnList.vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+import { ColumnProps } from './components/ColumnList.vue'
+import { UserProps } from './components/GlobalHeader.vue'
+const currentUser: UserProps = {
+  isLogin: false
 }
-</style>
+const test: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }
+]
+</script>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default defineComponent({})
+</script>
+<template>
+  <div class="container">
+    <GlobalHeader></GlobalHeader>
+    <ColumnList :list="test"> </ColumnList>
+  </div>
+</template>
+<style></style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number
+  title: string
+  avatar?: string
+  description: string
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>
+    }
+  },
+  setup(props) {
+    return props.list?.map(item => {
+      if (!item.avatar) {
+        item.avatar = require('.././assets/column.jpg')
+      }
+    })
+  }
+})
+</script>
+<template>
+  <ul class="box">
+    <li v-for="column in list" :key="column.id" class="card h-100 shadow-sm">
+      <img :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" />
+      <h5 class="card-title">{{ column.title }}</h5>
+      <p class="card-text text-left">{{ column.description }}</p>
+      <a href="#" class="btn btn-outline-primary">进入专栏</a>
+    </li>
+  </ul>
+</template>
+<style scoped>
+.box {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px 20px;
+  /* grid-template-rows: 1fr 1fr 1fr; */
+}
+li {
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+}
+li img,
+li a {
+  justify-self: center;
+}
+li h5 {
+  justify-self: center;
+  align-self: end;
+}
+</style>
